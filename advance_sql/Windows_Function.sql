22c1472d2eb5ef5e69d50c0c32f05463db2b01bc
nikhilsql5 <nikhilsql5@gmail.com>
2025-10-11 20:50:41 +0530

    Create Windows_Function.sql

---
+++ advance_sql/Windows_Function.sql
@@ -0,0 +1,14 @@
+USE AMAZON
+
+SELECT TOP 5
+c.cid,
+c.fname +' '+c.lname [Customer Name],
+p.product_name,
+p.price,
+o.order_date,
+o.order_status
+FROM Orders o
+FULL JOIN Customer c
+ON c.cid = o.cid
+FULL JOIN Product p
+ON p.pid = o.pid

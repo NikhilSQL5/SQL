59b03eb0aaab22743b4edf065913774ab8d9624c
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-03 09:40:24 +0530

    Add SQL query for customers with non-zero scores
    Added a query to retrieve customers with a score not equal to 0.

--- basic SQL/select.sql
+++ basic SQL/select.sql
@@ -40,3 +40,12 @@ SELECT
     country, 
     score
 FROM customers
+
+/* ==============================================================================
+   WHERE
+=============================================================================== */
+
+-- Retrieve customers with a score not equal to 0
+SELECT *
+FROM customers
+WHERE score != 0

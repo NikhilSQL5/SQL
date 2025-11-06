444b8f0f1b19a622123d1413123f928283686446
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-06 21:17:50 +0530

    Insert sample data into customers table
    Add initial data to customers table.

--- basic SQL/create_table.sql
+++ basic SQL/create_table.sql
@@ -1,6 +1,17 @@
+
 CREATE TABLE customers (
    id INT,
    first_name VARCHAR(50),
    country VARCHAR(50),
    score INT
 );
+
+INSERT INTO customers (id, first_name, country, score)
+VALUES 
+(1, 'Radha', 'Mumbai', 350),
+(2, 'Nick', 'USA', 800),
+(3, 'Ram', 'Delhi', 600),
+(4, 'Shyam', 'Poland', 540),
+(5, 'Prity', 'UK', 0),
+(6, 'Ramesh', 'India', 350),
+(7, NULL, NULL, NULL);

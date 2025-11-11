922418e18f5075c6553b0a3c037e7edd2fd98cdd
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-11 22:14:05 +0530

    Add query for customers with non-zero scores
    Added a query to retrieve customers with non-zero scores, sorted by score in descending order.

--- basic SQL/select.sql
+++ basic SQL/select.sql
@@ -76,3 +76,14 @@ ORDER BY country ASC
 SELECT *
 FROM customers
 ORDER BY country ASC, score DESC
+
+/* Retrieve the name, country, and score of customers 
+   whose score is not equal to 0
+   and sort the results by the highest score first. */
+SELECT
+    first_name,
+    country,
+    score
+FROM customers
+WHERE score != 0
+ORDER BY score DESC

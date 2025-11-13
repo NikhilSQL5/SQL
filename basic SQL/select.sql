34bf33f60b2b87ead57855b84d2bda8964b53f89
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-13 21:38:23 +0530

    Clarify SQL GROUP BY usage with comments
    Added a comment explaining the SQL grouping issue with 'first_name'.

--- basic SQL/select.sql
+++ basic SQL/select.sql
@@ -98,3 +98,12 @@ SELECT
     SUM(score) AS total_score
 FROM customers
 GROUP BY country
+
+/* This will not work because 'first_name' is neither part of the GROUP BY 
+   nor wrapped in an aggregate function. SQL doesn't know how to handle this column. */
+SELECT 
+    country,
+    first_name,
+    SUM(score) AS total_score
+FROM customers
+GROUP BY country

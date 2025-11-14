e394912b937624210f2da0399b574b45585b2eae
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-14 13:37:26 +0530

    Add total score and customer count query
    Added query to calculate total score and customer count per country.

--- basic SQL/select.sql
+++ basic SQL/select.sql
@@ -107,3 +107,11 @@ SELECT
     SUM(score) AS total_score
 FROM customers
 GROUP BY country
+
+-- Find the total score and total number of customers for each country
+SELECT 
+    country,
+    SUM(score) AS total_score,
+    COUNT(id) AS total_customers
+FROM customers
+GROUP BY country

aa8137731b6aea9486bf99b8a54c8aec7fe49a92
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-15 15:54:34 +0530

    Add query for average score by country
    Added a query to find average score per country with a filter.

--- basic SQL/select.sql
+++ basic SQL/select.sql
@@ -115,3 +115,12 @@ SELECT
     COUNT(id) AS total_customers
 FROM customers
 GROUP BY country
+
+/* Find the average score for each country
+   and return only those countries with an average score greater than 430 */
+SELECT
+    country,
+    AVG(score) AS avg_score
+FROM customers
+GROUP BY country
+HAVING AVG(score) > 430

9d506c07d3b5b9316802e9c00b6c4cad3efee2b2
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-17 20:50:39 +0530

    Update select.sql

--- basic SQL/select.sql
+++ basic SQL/select.sql
@@ -135,3 +135,11 @@ FROM customers
 WHERE score != 0
 GROUP BY country
 HAVING AVG(score) > 430
+
+/* ==============================================================================
+   DISTINCT
+=============================================================================== */
+
+-- Return Unique list of all countries
+SELECT DISTINCT country
+FROM customers

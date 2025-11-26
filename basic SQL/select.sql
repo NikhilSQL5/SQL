35fc65fc4fecf241ca7c24157eed255abb6dcf45
nikhilsql5 <nikhilsql5@gmail.com>
2025-11-26 21:44:23 +0530

    Add static string selection example to SQL file

--- basic SQL/select.sql
+++ basic SQL/select.sql
@@ -196,3 +196,5 @@ SELECT * FROM orders;
 /* Selecting Static Data */
 -- Select a static or constant value without accessing any table
 SELECT 123 AS static_number;
+
+SELECT 'Hello' AS static_string;

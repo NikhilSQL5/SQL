a3be626e949cff211acb6fdf734a1589ab836498
nikhilsql5 <nikhilsql5@gmail.com>
2025-12-10 09:34:41 +0530

    Update customer scores and country in SQL

--- basic SQL/DML.sql
+++ basic SQL/DML.sql
@@ -63,3 +63,9 @@ FROM customers
 UPDATE customers
 SET score = 0
 WHERE id = 6
+
+-- Change the score of customer with ID 10 to 0 and update the country to 'UK'
+UPDATE customers
+SET score = 0,
+    country = 'UK'
+WHERE id = 10

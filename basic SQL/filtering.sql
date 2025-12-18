d219dab79904905e7f061e411513fbbe90101b44
nikhilsql5 <nikhilsql5@gmail.com>
2025-12-18 11:36:05 +0530

    Add SQL filtering techniques overview document
    This document provides an overview of SQL filtering techniques using WHERE and various operators for precise data retrieval.

---
+++ basic SQL/filtering.sql
@@ -0,0 +1,19 @@
+/* ============================================================================== 
+   SQL Filtering Data
+-------------------------------------------------------------------------------
+   This document provides an overview of SQL filtering techniques using WHERE 
+   and various operators for precise data retrieval.
+
+   Table of Contents:
+     1. Comparison Operators
+        - =, <>, >, >=, <, <=
+     2. Logical Operators
+        - AND, OR, NOT
+     3. Range Filtering
+        - BETWEEN
+     4. Set Filtering
+        - IN
+     5. Pattern Matching
+        - LIKE
+=================================================================================
+*/

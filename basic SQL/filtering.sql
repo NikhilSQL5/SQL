038abdd75500452c34dc000207554914c3028d62
nikhilsql5 <nikhilsql5@gmail.com>
2025-12-25 09:21:20 +0530

    Add SQL queries for customer filtering
    Added SQL queries for filtering customers based on score and country.

--- basic SQL/filtering.sql
+++ basic SQL/filtering.sql
@@ -51,3 +51,14 @@ WHERE score >= 500
 SELECT *
 FROM customers
 WHERE score <= 500
+
+/* ============================================================================== 
+   LOGICAL OPERATORS
+=============================================================================== */
+
+/* Combining conditions using AND, OR, and NOT */
+
+-- Retrieve all customers who are from the USA and have a score greater than 500.
+SELECT *
+FROM customers
+WHERE country = 'USA' AND score > 500

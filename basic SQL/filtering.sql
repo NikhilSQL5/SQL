90cf0c081435733258d5b0dd322cd276991c196a
nikhilsql5 <nikhilsql5@gmail.com>
2025-12-26 08:52:56 +0530

    Add query to retrieve customers from USA or with high score

--- basic SQL/filtering.sql
+++ basic SQL/filtering.sql
@@ -62,3 +62,8 @@ WHERE score <= 500
 SELECT *
 FROM customers
 WHERE country = 'USA' AND score > 500
+
+-- Retrieve all customers who are either from the USA or have a score greater than 500.
+SELECT *
+FROM customers
+WHERE country = 'USA' OR score > 500

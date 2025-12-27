db5cab00a9ae1573368cc264d80927828f3e4f2e
nikhilsql5 <nikhilsql5@gmail.com>
2025-12-27 10:52:42 +0530

    Add query for customers with score >= 500
    Added a query to retrieve customers with a score not less than 500.

--- basic SQL/filtering.sql
+++ basic SQL/filtering.sql
@@ -67,3 +67,8 @@ WHERE country = 'USA' AND score > 500
 SELECT *
 FROM customers
 WHERE country = 'USA' OR score > 500
+
+-- Retrieve all customers with a score not less than 500.
+SELECT *
+FROM customers
+WHERE NOT score < 500

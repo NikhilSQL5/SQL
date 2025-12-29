f060a541ec70e14152eca0bb2e196a814ee56701
nikhilsql5 <nikhilsql5@gmail.com>
2025-12-30 05:09:35 +0530

    Add SQL query to filter customers by country

--- basic SQL/filtering.sql
+++ basic SQL/filtering.sql
@@ -86,3 +86,12 @@ WHERE score BETWEEN 100 AND 500
 SELECT *
 FROM customers
 WHERE score >= 100 AND score <= 500
+
+/* ============================================================================== 
+   SET FILTERING - IN
+=============================================================================== */
+
+-- Retrieve all customers from either Germany or the USA.
+SELECT *
+FROM customers
+WHERE country IN ('Germany', 'USA')

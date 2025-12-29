a81afcf3a31ca0771838783b75eb8c850935095a
nikhilsql5 <nikhilsql5@gmail.com>
2025-12-29 09:00:01 +0530

    Include alternative method for score filtering
    Added an alternative method to retrieve customers based on score range.

--- basic SQL/filtering.sql
+++ basic SQL/filtering.sql
@@ -81,3 +81,8 @@ WHERE NOT score < 500
 SELECT *
 FROM customers
 WHERE score BETWEEN 100 AND 500
+
+-- Alternative method (Equivalent to BETWEEN)
+SELECT *
+FROM customers
+WHERE score >= 100 AND score <= 500

db872b1b80437d011fbf9ec6927ae34ae5dfc037
nikhilsql5 <nikhilsql5@gmail.com>
2026-01-06 10:53:25 +0530

    Update and rename joins to joins.sql

--- basic SQL/joins.sql
+++ basic SQL/joins.sql
@@ -19,3 +19,12 @@
      3. Multiple Table Joins (4 Tables)
 =================================================================================
 */
+
+/* ============================================================================== 
+   BASIC JOINS 
+=============================================================================== */
+
+-- No Join
+/* Retrieve all data from customers and orders as separate results */
+SELECT * FROM customers;
+SELECT * FROM orders;

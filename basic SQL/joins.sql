937d5a09ae25c08714896ab0b9fa5b50c0e594c3
nikhilsql5 <nikhilsql5@gmail.com>
2026-01-07 17:55:42 +0530

    Update joins.sql

--- basic SQL/joins.sql
+++ basic SQL/joins.sql
@@ -28,3 +28,15 @@
 /* Retrieve all data from customers and orders as separate results */
 SELECT * FROM customers;
 SELECT * FROM orders;
+
+-- INNER JOIN
+/* Get all customers along with their orders, 
+   but only for customers who have placed an order */
+SELECT
+    c.id,
+    c.first_name,
+    o.order_id,
+    o.sales
+FROM customers AS c
+INNER JOIN orders AS o
+ON c.id = o.customer_id

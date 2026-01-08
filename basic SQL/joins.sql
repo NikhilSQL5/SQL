0ecaa9cb023a59f603c16795a6b9b8a0cdd4dc39
nikhilsql5 <nikhilsql5@gmail.com>
2026-01-08 08:55:04 +0530

    Add LEFT JOIN example for customers and orders
    Added LEFT JOIN query to retrieve all customers and their orders, including those without orders.

--- basic SQL/joins.sql
+++ basic SQL/joins.sql
@@ -40,3 +40,15 @@ SELECT
 FROM customers AS c
 INNER JOIN orders AS o
 ON c.id = o.customer_id
+
+-- LEFT JOIN
+/* Get all customers along with their orders, 
+   including those without orders */
+SELECT
+    c.id,
+    c.first_name,
+    o.order_id,
+    o.sales
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id

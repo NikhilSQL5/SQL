e779f604b107e2236920229fb859c97646885f9f
nikhilsql5 <nikhilsql5@gmail.com>
2026-01-10 18:37:00 +0530

    Add RIGHT JOIN example to SQL joins file
    Added RIGHT JOIN example to demonstrate fetching all orders with or without matching customers.

--- basic SQL/joins.sql
+++ basic SQL/joins.sql
@@ -52,3 +52,17 @@ SELECT
 FROM customers AS c
 LEFT JOIN orders AS o
 ON c.id = o.customer_id
+
+
+-- RIGHT JOIN
+/* Get all customers along with their orders, 
+   including orders without matching customers */
+SELECT
+    c.id,
+    c.first_name,
+    o.order_id,
+    o.customer_id,
+    o.sales
+FROM customers AS c 
+RIGHT JOIN orders AS o 
+ON c.id = o.customer_id

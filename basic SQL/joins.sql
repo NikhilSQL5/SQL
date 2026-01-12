988dc2c79b479c7855d67adf7bc625d9810aa043
nikhilsql5 <nikhilsql5@gmail.com>
2026-01-12 09:25:57 +0530

    Add FULL JOIN query for customers and orders
    Added a FULL JOIN query to retrieve all customers and orders.

--- basic SQL/joins.sql
+++ basic SQL/joins.sql
@@ -78,3 +78,15 @@ SELECT
 FROM orders AS o 
 LEFT JOIN customers AS c
 ON c.id = o.customer_id
+
+-- FULL JOIN
+/* Get all customers and all orders, even if there’s no match */
+SELECT
+    c.id,
+    c.first_name,
+    o.order_id,
+    o.customer_id,
+    o.sales
+FROM customers AS c 
+FULL JOIN orders AS o 
+ON c.id = o.customer_id

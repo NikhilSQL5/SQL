d0d43fbcdc79acd26acd48850df1a6a867776d74
nikhilsql5 <nikhilsql5@gmail.com>
2026-01-13 20:42:34 +0530

    Add LEFT ANTI JOIN for customers without orders
    Added a LEFT ANTI JOIN query to retrieve customers without orders.

--- basic SQL/joins.sql
+++ basic SQL/joins.sql
@@ -90,3 +90,15 @@ SELECT
 FROM customers AS c 
 FULL JOIN orders AS o 
 ON c.id = o.customer_id
+
+/* ============================================================================== 
+   ADVANCED JOINS
+=============================================================================== */
+
+-- LEFT ANTI JOIN
+/* Get all customers who haven't placed any order */
+SELECT *
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL

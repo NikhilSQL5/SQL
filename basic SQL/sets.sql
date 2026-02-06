c9d380dd35f286a7848a4b4ab29bb9b5d21c8de7
nikhilsql5 <nikhilsql5@gmail.com>
2026-02-06 21:54:19 +0530

    Add UNION ALL query for Employees and Customers
    Combined data from Employees and Customers using UNION ALL.

--- basic SQL/sets.sql
+++ basic SQL/sets.sql
@@ -101,3 +101,16 @@ SELECT
     FirstName,
     LastName
 FROM Sales.Employees;
+
+/* TASK 2: 
+   Combine the data from Employees and Customers into one table, including duplicates, using UNION ALL 
+*/
+SELECT
+    FirstName,
+    LastName
+FROM Sales.Customers
+UNION ALL
+SELECT
+    FirstName,
+    LastName
+FROM Sales.Employees;

4b32c144c10c154bc0f5525ced4c1919a56fb5a0
nikhilsql5 <nikhilsql5@gmail.com>
2026-02-13 21:58:32 +0530

    Add query to convert first name to uppercase
    Added a query to convert first names to uppercase.

--- basic SQL/string_functions.sql
+++ basic SQL/string_functions.sql
@@ -37,3 +37,8 @@ FROM customers
 SELECT 
     LOWER(first_name) AS lower_case_name
 FROM customers
+
+-- Convert the first name to uppercase
+SELECT 
+    UPPER(first_name) AS upper_case_name
+FROM customers

6b3589fcbad01663d252d34a1aae472a5c5e2ddc
nikhilsql5 <nikhilsql5@gmail.com>
2026-02-17 08:01:29 +0530

    Add SQL example to replace file extension from txt to csv

--- basic SQL/string_functions.sql
+++ basic SQL/string_functions.sql
@@ -63,3 +63,8 @@ WHERE LEN(first_name)  != LEN(TRIM(first_name))
 SELECT
 '123-456-7890' AS phone,
 REPLACE('123-456-7890', '-', '/') AS clean_phone
+
+-- Replace File Extence from txt to csv
+SELECT
+'report.txt' AS old_filename,
+REPLACE('report.txt', '.txt', '.csv') AS new_filename

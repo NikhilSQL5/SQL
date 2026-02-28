86103168c622e58e26a311263d749fc4c54d1bb6
nikhilsql5 <nikhilsql5@gmail.com>
2026-02-28 22:31:45 +0530

    Add SQL query to display OrderID and dates

--- basic SQL/date_functions.sql
+++ basic SQL/date_functions.sql
@@ -19,3 +19,17 @@
     10. ISDATE
 ===============================================================================
 */
+
+/* ==============================================================================
+   GETDATE() | DATE VALUES
+===============================================================================*/
+
+/* TASK 1:
+   Display OrderID, CreationTime, a hard-coded date, and the current system date.
+*/
+SELECT
+    OrderID,
+    CreationTime,
+    '2025-08-20' AS HardCoded,
+    GETDATE() AS Today
+FROM Sales.Orders;

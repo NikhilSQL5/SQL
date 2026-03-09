3b65c18aa88d7b739a9dcda3d5cbcad3148cb80a
nikhilsql5 <nikhilsql5@gmail.com>
2026-03-09 21:50:20 +0530

    Add query to count orders by year

--- basic SQL/date_functions.sql
+++ basic SQL/date_functions.sql
@@ -107,3 +107,16 @@ SELECT
     CreationTime,
     EOMONTH(CreationTime) AS EndOfMonth
 FROM Sales.Orders;
+
+/* ==============================================================================
+   DATE PARTS | USE CASES
+===============================================================================*/
+
+/* TASK 5:
+   How many orders were placed each year?
+*/
+SELECT 
+    YEAR(OrderDate) AS OrderYear, 
+    COUNT(*) AS TotalOrders
+FROM Sales.Orders
+GROUP BY YEAR(OrderDate);

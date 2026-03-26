5c413eb0558101c3ea9a826354e756dcfb4deb04
nikhilsql5 <nikhilsql5@gmail.com>
2026-03-26 23:31:05 +0530

    Add query for monthly orders with friendly names

--- basic SQL/date_functions.sql
+++ basic SQL/date_functions.sql
@@ -142,3 +142,12 @@ SELECT
     COUNT(*) AS TotalOrders
 FROM Sales.Orders
 GROUP BY MONTH(OrderDate);
+
+/* TASK 7:
+   How many orders were placed each month (using friendly month names)?
+*/
+SELECT 
+    DATENAME(month, OrderDate) AS OrderMonth, 
+    COUNT(*) AS TotalOrders
+FROM Sales.Orders
+GROUP BY DATENAME(month, OrderDate);

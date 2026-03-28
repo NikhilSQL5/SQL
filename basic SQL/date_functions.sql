b66e4a3c3e0682c1d6934d71c88d84d1e63a72f3
nikhilsql5 <nikhilsql5@gmail.com>
2026-03-28 22:26:20 +0530

    Add query for February orders
    Add query to show all orders placed in February.

--- basic SQL/date_functions.sql
+++ basic SQL/date_functions.sql
@@ -160,3 +160,11 @@ SELECT
     COUNT(*) AS TotalOrders
 FROM Sales.Orders
 GROUP BY DATENAME(month, OrderDate);
+
+/* TASK 8:
+   Show all orders that were placed during the month of February.
+*/
+SELECT
+    *
+FROM Sales.Orders
+WHERE MONTH(OrderDate) = 2;

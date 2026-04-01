28be93660d77b65b570ddf417a12d83bfafadaa4
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-01 20:45:49 +0530

    Add CONVERT function demonstration in SQL
    Added demonstration of CONVERT function for various data type conversions.

--- basic SQL/date_functions.sql
+++ basic SQL/date_functions.sql
@@ -209,3 +209,19 @@ SELECT
     COUNT(*) AS TotalOrders
 FROM Sales.Orders
 GROUP BY FORMAT(CreationTime, 'MMM yy');
+
+/* ==============================================================================
+   CONVERT()
+===============================================================================*/
+
+/* TASK 12:
+   Demonstrate conversion using CONVERT.
+*/
+SELECT
+    CONVERT(INT, '123') AS [String to Int CONVERT],
+    CONVERT(DATE, '2025-08-20') AS [String to Date CONVERT],
+    CreationTime,
+    CONVERT(DATE, CreationTime) AS [Datetime to Date CONVERT],
+    CONVERT(VARCHAR, CreationTime, 32) AS [USA Std. Style:32],
+    CONVERT(VARCHAR, CreationTime, 34) AS [EURO Std. Style:34]
+FROM Sales.Orders;

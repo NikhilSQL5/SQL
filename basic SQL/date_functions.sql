60302024c128224838db14a356d2e0fff7b44f1a
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-03 19:59:19 +0530

    Implement date arithmetic examples in SQL
    Add examples of DATEADD and DATEDIFF functions for date arithmetic.

--- basic SQL/date_functions.sql
+++ basic SQL/date_functions.sql
@@ -241,3 +241,18 @@ SELECT
     CreationTime,
     CAST(CreationTime AS DATE) AS [Datetime to Date]
 FROM Sales.Orders;
+
+/* ==============================================================================
+   DATEADD() / DATEDIFF()
+===============================================================================*/
+
+/* TASK 14:
+   Perform date arithmetic on OrderDate.
+*/
+SELECT
+    OrderID,
+    OrderDate,
+    DATEADD(day, -10, OrderDate) AS TenDaysBefore,
+    DATEADD(month, 3, OrderDate) AS ThreeMonthsLater,
+    DATEADD(year, 2, OrderDate) AS TwoYearsLater
+FROM Sales.Orders;

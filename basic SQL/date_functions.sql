f0d472cf5a59aa1e1279b28308aaed9cf360a721
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-05 10:26:53 +0530

    Add average shipping duration query for each month
    Added a query to calculate average shipping duration per month.

--- basic SQL/date_functions.sql
+++ basic SQL/date_functions.sql
@@ -265,3 +265,12 @@ SELECT
     BirthDate,
     DATEDIFF(year, BirthDate, GETDATE()) AS Age
 FROM Sales.Employees;
+
+/* TASK 16:
+   Find the average shipping duration in days for each month.
+*/
+SELECT
+    MONTH(OrderDate) AS OrderMonth,
+    AVG(DATEDIFF(day, OrderDate, ShipDate)) AS AvgShip
+FROM Sales.Orders
+GROUP BY MONTH(OrderDate);

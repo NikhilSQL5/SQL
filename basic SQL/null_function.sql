74bcc85167d8ca70d6773962d7212b4a6a1c9cfe
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-12 13:26:17 +0530

    Add query to identify customers with non-null scores

--- basic SQL/null_function.sql
+++ basic SQL/null_function.sql
@@ -70,3 +70,10 @@ FROM Sales.Orders;
 SELECT *
 FROM Sales.Customers
 WHERE Score IS NULL;
+
+/* TASK 6: 
+   Identify the customers who have scores 
+*/
+SELECT *
+FROM Sales.Customers
+WHERE Score IS NOT NULL;

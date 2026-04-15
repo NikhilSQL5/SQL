cd5465427f223fdd4cb12536715d8d131e036245
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-15 19:56:44 +0530

    Update case_statement.sql

--- basic SQL/case_statement.sql
+++ basic SQL/case_statement.sql
@@ -25,3 +25,22 @@ FROM (
 ) AS t
 GROUP BY Category
 ORDER BY TotalSales DESC;
+
+/* ==============================================================================
+   USE CASE: MAPPING
+===============================================================================*/
+
+/* TASK 2: 
+   Retrieve customer details with abbreviated country codes 
+*/
+SELECT
+    CustomerID,
+    FirstName,
+    LastName,
+    Country,
+    CASE 
+        WHEN Country = 'Germany' THEN 'DE'
+        WHEN Country = 'USA'     THEN 'US'
+        ELSE 'n/a'
+    END AS CountryAbbr
+FROM Sales.Customers;

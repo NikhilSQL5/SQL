07245e0a834e86b57bb8e0d3c3ad62bff6366887
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-17 21:58:50 +0530

    Add NULL handling for average customer score calculation
    Added a query to calculate average customer scores while treating NULL values as 0, including CustomerID and LastName.

--- basic SQL/case_statement.sql
+++ basic SQL/case_statement.sql
@@ -68,3 +68,29 @@ SELECT
         ELSE 'n/a'
     END AS CountryAbbr2
 FROM Sales.Customers;
+
+/* ==============================================================================
+   HANDLING NULLS
+===============================================================================*/
+
+/* TASK 4: 
+   Calculate the average score of customers, treating NULL as 0,
+   and provide CustomerID and LastName details.
+*/
+SELECT
+    CustomerID,
+    LastName,
+    Score,
+    CASE
+        WHEN Score IS NULL THEN 0
+        ELSE Score
+    END AS ScoreClean,
+    AVG(
+        CASE
+            WHEN Score IS NULL THEN 0
+            ELSE Score
+        END
+    ) OVER () AS AvgCustomerClean,
+    AVG(Score) OVER () AS AvgCustomer
+FROM Sales.Customers;
+

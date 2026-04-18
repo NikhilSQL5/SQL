4d0dd1e78ab4e31336efa1c148e09bec394e1fba
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-18 16:33:44 +0530

    Implement conditional aggregation for high sales orders
    Added a query to count orders with sales greater than 30.

--- basic SQL/case_statement.sql
+++ basic SQL/case_statement.sql
@@ -94,3 +94,21 @@ SELECT
     AVG(Score) OVER () AS AvgCustomer
 FROM Sales.Customers;
 
+/* ==============================================================================
+   CONDITIONAL AGGREGATION
+===============================================================================*/
+
+/* TASK 5: 
+   Count how many orders each customer made with sales greater than 30 
+*/
+SELECT
+    CustomerID,
+    SUM(
+        CASE
+            WHEN Sales > 30 THEN 1
+            ELSE 0
+        END
+    ) AS TotalOrdersHighSales,
+    COUNT(*) AS TotalOrders
+FROM Sales.Orders
+GROUP BY CustomerID;

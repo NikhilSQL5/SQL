66715842301f14be9d42a5bda5e3c8c3b8cb3bac
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-19 11:44:33 +0530

    Create aggregate_function.sql for customer count
    Add SQL script for basic aggregate functions including customer count.

---
+++ basic SQL/aggregate_function.sql
@@ -0,0 +1,7 @@
+/* ============================================================================== 
+   BASIC AGGREGATE FUNCTIONS
+=============================================================================== */
+
+-- Find the total number of customers
+SELECT COUNT(*) AS total_customers
+FROM customers

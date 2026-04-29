5bd56fb0808250f218781d69c9c146e3153a5ef6
nikhilsql5 <nikhilsql5@gmail.com>
2026-04-29 20:09:43 +0530

    Enhance SQL with window functions for sales analysis
    Added SQL query to calculate total sales and sales by product using window functions.

--- basic SQL/windows_function.sql
+++ basic SQL/windows_function.sql
@@ -53,3 +53,20 @@ SELECT
     Sales,
     SUM(Sales) OVER () AS Total_Sales
 FROM Sales.Orders;
+
+/* ==============================================================================
+   SQL WINDOW FUNCTIONS | PARTITION CLAUSE
+===============================================================================*/
+
+/* TASK 4: 
+   Find the total sales across all orders and for each product,
+   additionally providing details such as OrderID and OrderDate 
+*/
+SELECT
+    OrderID,
+    OrderDate,
+    ProductID,
+    Sales,
+    SUM(Sales) OVER () AS Total_Sales,
+    SUM(Sales) OVER (PARTITION BY ProductID) AS Sales_By_Product
+FROM Sales.Orders;

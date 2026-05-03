f1916a32e6eb643a0753bb5e444ad3f6e297485c
nikhilsql5 <nikhilsql5@gmail.com>
2026-05-03 21:36:42 +0530

    Add window function for total sales by order status
    Added a query to calculate total sales by order status for current and next two orders using window functions.

--- basic SQL/windows_function.sql
+++ basic SQL/windows_function.sql
@@ -99,3 +99,23 @@ SELECT
     Sales,
     RANK() OVER (ORDER BY Sales DESC) AS Rank_Sales
 FROM Sales.Orders;
+
+/* ==============================================================================
+   SQL WINDOW FUNCTIONS | FRAME CLAUSE
+===============================================================================*/
+
+/* TASK 7: 
+   Calculate Total Sales by Order Status for current and next two orders 
+*/
+SELECT
+    OrderID,
+    OrderDate,
+    ProductID,
+    OrderStatus,
+    Sales,
+    SUM(Sales) OVER (
+        PARTITION BY OrderStatus 
+        ORDER BY OrderDate 
+        ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING
+    ) AS Total_Sales
+FROM Sales.Orders;

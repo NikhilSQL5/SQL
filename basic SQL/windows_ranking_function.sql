00b333d8622de4f5f68e04ecad02e5bb71cf8ee0
nikhilsql5 <nikhilsql5@gmail.com>
2026-05-22 23:03:21 +0530

    Implement CUME_DIST to identify top 40% priced products
    Add SQL query to find products in the highest 40% of prices using CUME_DIST.

--- basic SQL/windows_ranking_function.sql
+++ basic SQL/windows_ranking_function.sql
@@ -120,3 +120,25 @@ SELECT
     NTILE(5) OVER (ORDER BY OrderID) AS Buckets,
     *
 FROM Sales.Orders;
+
+
+/* ============================================================
+   SQL WINDOW RANKING | CUME_DIST
+   ============================================================ */
+
+/* TASK 9:
+   Find Products that Fall Within the Highest 40% of the Prices
+*/
+SELECT 
+    Product,
+    Price,
+    DistRank,
+    CONCAT(DistRank * 100, '%') AS DistRankPerc
+FROM (
+    SELECT
+        Product,
+        Price,
+        CUME_DIST() OVER (ORDER BY Price DESC) AS DistRank
+    FROM Sales.Products
+) AS PriceDistribution
+WHERE DistRank <= 0.4;

94737f6404c498c82b85457ce644391ee0228fbe
nikhilsql5 <nikhilsql5@gmail.com>
2026-05-26 22:33:20 +0530

    Implement SQL window functions for sales analysis
    Add SQL query to find lowest and highest sales for each product.

--- basic SQL/windows_value_function.sql
+++ basic SQL/windows_value_function.sql
@@ -61,3 +61,24 @@ FROM (
     FROM Sales.Orders
 ) AS CustomerOrdersWithNext
 GROUP BY CustomerID;
+
+/* ============================================================
+   SQL WINDOW VALUE | FIRST & LAST VALUE
+   ============================================================ */
+
+/* TASK 3:
+   Find the Lowest and Highest Sales for Each Product,
+   and determine the difference between the current Sales and the lowest Sales for each Product.
+*/
+SELECT
+    OrderID,
+    ProductID,
+    Sales,
+    FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales) AS LowestSales,
+    LAST_VALUE(Sales) OVER (
+        PARTITION BY ProductID 
+        ORDER BY Sales 
+        ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
+    ) AS HighestSales,
+    Sales - FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales) AS SalesDifference
+FROM Sales.Orders;

47aec183b798dec0cbf8e651e013a3159855c2f4
nikhilsql5 <nikhilsql5@gmail.com>
2026-06-01 21:34:41 +0530

    Update subqueries.sql

--- basic SQL/subqueries.sql
+++ basic SQL/subqueries.sql
@@ -37,3 +37,23 @@ SELECT
     OrderID,
     OrderDate
 FROM Sales.Orders;
+
+/* ==============================================================================
+   SUBQUERY | FROM CLAUSE
+===============================================================================*/
+
+/* TASK 1:
+   Find the products that have a price higher than the average price of all products.
+*/
+-- Main Query
+SELECT
+*
+FROM (
+    -- Subquery
+    SELECT
+        ProductID,
+        Price,
+        AVG(Price) OVER () AS AvgPrice
+    FROM Sales.Products
+) AS t
+WHERE Price > AvgPrice;

f5c3f4c3566813454a4947f6c049b90d047d9210
nikhilsql5 <nikhilsql5@gmail.com>
2026-06-13 23:05:09 +0530

    Implement query for orders excluding German customers
    Add query to show orders made by customers not in Germany.

--- basic SQL/subqueries.sql
+++ basic SQL/subqueries.sql
@@ -177,3 +177,18 @@ WHERE CustomerID IN (
     FROM Sales.Customers
     WHERE Country = 'Germany'
 );
+
+/* TASK 8:
+   Show the details of orders made by customers not in Germany.
+*/
+-- Main Query
+SELECT
+    *
+FROM Sales.Orders
+WHERE CustomerID NOT IN (
+    -- Subquery
+    SELECT
+        CustomerID
+    FROM Sales.Customers
+    WHERE Country = 'Germany'
+);

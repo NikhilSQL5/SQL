94233d89aab08659aeaa1ee99a8bafd0d3a6a0b3
nikhilsql5 <nikhilsql5@gmail.com>
2026-06-18 21:29:35 +0530

    Add SQL script for Common Table Expressions (CTEs)
    This script demonstrates the use of Common Table Expressions (CTEs) in SQL Server, including examples of non-recursive and recursive CTEs for data aggregation, segmentation, and hierarchical data.

---
+++ basic SQL/CTE.sql
@@ -0,0 +1,76 @@
+/* ==============================================================================
+   SQL Common Table Expressions (CTEs)
+-------------------------------------------------------------------------------
+   This script demonstrates the use of Common Table Expressions (CTEs) in SQL Server.
+   It includes examples of non-recursive CTEs for data aggregation and segmentation,
+   as well as recursive CTEs for generating sequences and building hierarchical data.
+
+   Table of Contents:
+     1. NON-RECURSIVE CTE
+     2. RECURSIVE CTE | GENERATE SEQUENCE
+     3. RECURSIVE CTE | BUILD HIERARCHY
+===============================================================================*/
+
+/* ==============================================================================
+   NON-RECURSIVE CTE
+===============================================================================*/
+
+
+-- Step1: Find the total Sales Per Customer (Standalone CTE)
+WITH CTE_Total_Sales AS
+(
+    SELECT
+        CustomerID,
+        SUM(Sales) AS TotalSales
+    FROM Sales.Orders
+    GROUP BY CustomerID
+)
+-- Step2: Find the last order date for each customer (Standalone CTE)
+, CTE_Last_Order AS
+(
+    SELECT
+        CustomerID,
+        MAX(OrderDate) AS Last_Order
+    FROM Sales.Orders
+    GROUP BY CustomerID
+)
+-- Step3: Rank Customers based on Total Sales Per Customer (Nested CTE)
+, CTE_Customer_Rank AS
+(
+    SELECT
+        CustomerID,
+        TotalSales,
+        RANK() OVER (ORDER BY TotalSales DESC) AS CustomerRank
+    FROM CTE_Total_Sales
+)
+-- Step4: segment customers based on their total sales (Nested CTE)
+, CTE_Customer_Segments AS
+(
+    SELECT
+        CustomerID,
+        TotalSales,
+        CASE 
+            WHEN TotalSales > 100 THEN 'High'
+            WHEN TotalSales > 80  THEN 'Medium'
+            ELSE 'Low'
+        END AS CustomerSegments
+    FROM CTE_Total_Sales
+)
+-- Main Query
+SELECT
+    c.CustomerID,
+    c.FirstName,
+    c.LastName,
+    cts.TotalSales,
+    clo.Last_Order,
+    ccr.CustomerRank,
+    ccs.CustomerSegments
+FROM Sales.Customers AS c
+LEFT JOIN CTE_Total_Sales AS cts
+    ON cts.CustomerID = c.CustomerID
+LEFT JOIN CTE_Last_Order AS clo
+    ON clo.CustomerID = c.CustomerID
+LEFT JOIN CTE_Customer_Rank AS ccr
+    ON ccr.CustomerID = c.CustomerID
+LEFT JOIN CTE_Customer_Segments AS ccs
+    ON ccs.CustomerID = c.CustomerID;

08ae6040c657c90390fd021540f86f2d23e0ae90
nikhilsql5 <nikhilsql5@gmail.com>
2026-06-19 08:39:44 +0530

    Add CTE for generating number sequence from 1 to 20
    Added a Common Table Expression (CTE) to generate a sequence of numbers from 1 to 20.

--- basic SQL/CTE.sql
+++ basic SQL/CTE.sql
@@ -74,3 +74,19 @@ LEFT JOIN CTE_Customer_Rank AS ccr
     ON ccr.CustomerID = c.CustomerID
 LEFT JOIN CTE_Customer_Segments AS ccs
     ON ccs.CustomerID = c.CustomerID;
+
+/* TASK 2:
+   Generate a sequence of numbers from 1 to 20.
+*/
+WITH Series AS (
+    -- Anchor Query
+    SELECT 1 AS MyNumber
+    UNION ALL
+    -- Recursive Query
+    SELECT MyNumber + 1
+    FROM Series
+    WHERE MyNumber < 20
+)
+-- Main Query
+SELECT *
+FROM Series

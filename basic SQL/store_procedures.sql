14142ee09549034aacca758560ad9b2306b1dfc0
nikhilsql5 <nikhilsql5@gmail.com>
2026-07-08 10:18:41 +0530

    Implement error handling in GetCustomerSummary procedure
    Added error handling using TRY CATCH in GetCustomerSummary stored procedure.

--- basic SQL/store_procedures.sql
+++ basic SQL/store_procedures.sql
@@ -194,3 +194,71 @@ GO
 EXEC GetCustomerSummary @Country = 'Germany';
 EXEC GetCustomerSummary @Country = 'USA';
 EXEC GetCustomerSummary;
+
+/* ==============================================================================
+   Error Handling TRY CATCH in Stored Procedure
+============================================================================== */
+
+ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA' AS
+    
+BEGIN
+    BEGIN TRY
+        -- Declare Variables
+        DECLARE @TotalCustomers INT, @AvgScore FLOAT;     
+
+        /* --------------------------------------------------------------------------
+           Prepare & Cleanup Data
+        -------------------------------------------------------------------------- */
+
+        IF EXISTS (SELECT 1 FROM Sales.Customers WHERE Score IS NULL AND Country = @Country)
+        BEGIN
+            PRINT('Updating NULL Scores to 0');
+            UPDATE Sales.Customers
+            SET Score = 0
+            WHERE Score IS NULL AND Country = @Country;
+        END
+        ELSE
+        BEGIN
+            PRINT('No NULL Scores found');
+        END;
+
+        /* --------------------------------------------------------------------------
+           Generating Reports
+        -------------------------------------------------------------------------- */
+        SELECT
+            @TotalCustomers = COUNT(*),
+            @AvgScore = AVG(Score)
+        FROM Sales.Customers
+        WHERE Country = @Country;
+
+        PRINT('Total Customers from ' + @Country + ':' + CAST(@TotalCustomers AS NVARCHAR));
+        PRINT('Average Score from ' + @Country + ':' + CAST(@AvgScore AS NVARCHAR));
+
+        SELECT
+            COUNT(OrderID) AS TotalOrders,
+            SUM(Sales) AS TotalSales,
+            1/0 AS FaultyCalculation  -- Intentional error for demonstration
+        FROM Sales.Orders AS o
+        JOIN Sales.Customers AS c
+            ON c.CustomerID = o.CustomerID
+        WHERE c.Country = @Country;
+    END TRY
+    BEGIN CATCH
+        /* --------------------------------------------------------------------------
+           Error Handling
+        -------------------------------------------------------------------------- */
+        PRINT('An error occurred.');
+        PRINT('Error Message: ' + ERROR_MESSAGE());
+        PRINT('Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR));
+        PRINT('Error Severity: ' + CAST(ERROR_SEVERITY() AS NVARCHAR));
+        PRINT('Error State: ' + CAST(ERROR_STATE() AS NVARCHAR));
+        PRINT('Error Line: ' + CAST(ERROR_LINE() AS NVARCHAR));
+        PRINT('Error Procedure: ' + ISNULL(ERROR_PROCEDURE(), 'N/A'));
+    END CATCH;
+END
+GO
+
+--Execute Stored Procedure
+EXEC GetCustomerSummary @Country = 'Germany';
+EXEC GetCustomerSummary @Country = 'USA';
+EXEC GetCustomerSummary;

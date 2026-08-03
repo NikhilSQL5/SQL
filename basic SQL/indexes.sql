8030556227848ed40ca40f2bd42acb5632b684c4
nikhilsql5 <nikhilsql5@gmail.com>
2026-08-03 22:10:23 +0530

    Update indexes.sql

--- basic SQL/indexes.sql
+++ basic SQL/indexes.sql
@@ -153,3 +153,8 @@ VALUES (106, 'Caps');
 SELECT *
 FROM Sales.Customers
 WHERE Country = 'USA';
+
+-- Create a Non-Clustered Filtered Index on the Country column for rows where Country = 'USA'
+CREATE NONCLUSTERED INDEX idx_Customers_Country
+ON Sales.Customers (Country)
+WHERE Country = 'USA';

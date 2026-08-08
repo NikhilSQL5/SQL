1934f45e2e88228103ce3e421f1565d869121095
nikhilsql5 <nikhilsql5@gmail.com>
2026-08-08 08:48:17 +0530

    Add query to monitor duplicate indexes
    Added a query to monitor duplicate indexes in the database.

--- basic SQL/indexes.sql
+++ basic SQL/indexes.sql
@@ -203,3 +203,19 @@ ORDER BY tbl.name, idx.name;
 
 SELECT * 
 FROM sys.dm_db_missing_index_details;
+
+/* ==============================================================================
+   Monitor Duplicate Indexes
+============================================================================== */
+
+SELECT  
+	tbl.name AS TableName,
+	col.name AS IndexColumn,
+	idx.name AS IndexName,
+	idx.type_desc AS IndexType,
+	COUNT(*) OVER (PARTITION BY  tbl.name , col.name ) ColumnCount
+FROM sys.indexes idx
+JOIN sys.tables tbl ON idx.object_id = tbl.object_id
+JOIN sys.index_columns ic ON idx.object_id = ic.object_id AND idx.index_id = ic.index_id
+JOIN sys.columns col ON ic.object_id = col.object_id AND ic.column_id = col.column_id
+ORDER BY ColumnCount DESC

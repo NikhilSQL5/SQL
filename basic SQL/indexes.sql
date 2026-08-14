afa450929355e857c590c63ee640bb88b4990d97
nikhilsql5 <nikhilsql5@gmail.com>
2026-08-14 21:21:49 +0530

    Add index fragmentation statistics query
    Added a query to retrieve index fragmentation statistics for the current database.

--- basic SQL/indexes.sql
+++ basic SQL/indexes.sql
@@ -249,3 +249,21 @@ GO
 -- Update statistics for all tables in the database
 EXEC sp_updatestats;
 GO
+
+/* ==============================================================================
+   Fragementations
+============================================================================== */
+
+-- Retrieve index fragmentation statistics for the current database
+SELECT 
+    tbl.name AS TableName,
+    idx.name AS IndexName,
+    s.avg_fragmentation_in_percent,
+    s.page_count
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS s
+INNER JOIN sys.tables tbl 
+    ON s.object_id = tbl.object_id
+INNER JOIN sys.indexes AS idx 
+    ON idx.object_id = s.object_id
+    AND idx.index_id = s.index_id
+ORDER BY s.avg_fragmentation_in_percent DESC;

5a2c214e33225d2abb5ee0431c720bc4d8a5c450
nikhilsql5 <nikhilsql5@gmail.com>
2026-08-18 10:38:39 +0530

    Add SQL script for demonstrating partitioning features
    This script demonstrates SQL Server partitioning features, including the creation of partition functions, filegroups, data files, partition schemes, and partitioned tables. It also includes verification queries and execution plan comparisons.

---
+++ basic SQL/partition.sql
@@ -0,0 +1,35 @@
+/* ==============================================================================
+   SQL Partitioning
+-------------------------------------------------------------------------------
+   This script demonstrates SQL Server partitioning features. It covers the
+   creation of partition functions, filegroups, data files, partition schemes,
+   partitioned tables, and verification queries. It also shows how to compare
+   execution plans between partitioned and non-partitioned tables.
+
+   Table of Contents:
+     1. Create a Partition Function
+     2. Create Filegroups
+     3. Create Data Files
+     4. Create Partition Scheme
+     5. Create the Partitioned Table
+     6. Insert Data Into the Partitioned Table
+     7. Verify Partitioning and Compare Execution Plans
+=================================================================================
+*/
+
+/* ==============================================================================
+   Step 1: Create a Partition Function
+============================================================================== */
+
+-- Create Left Range Partition Functions based on Years
+CREATE PARTITION FUNCTION PartitionByYear (DATE)
+AS RANGE LEFT FOR VALUES ('2023-12-31', '2024-12-31', '2025-12-31')
+
+-- Query lists all existing Partition Function
+SELECT 
+	name,
+	function_id,
+	type,
+	type_desc,
+	boundary_value_on_right
+FROM sys.partition_functions

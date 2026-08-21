cd5d71ee923b648bbd49f486b012a90ebd463417
nikhilsql5 <nikhilsql5@gmail.com>
2026-08-21 09:24:01 +0530

    Add query to list existing filegroups
    Added a query to list all existing filegroups in the database.

--- basic SQL/partition.sql
+++ basic SQL/partition.sql
@@ -46,3 +46,8 @@ ALTER DATABASE SalesDB ADD FILEGROUP FG_2026;
 
 -- Optional: Remove a Filegroup if needed
 ALTER DATABASE SalesDB REMOVE FILEGROUP FG_2023;
+
+-- Query: List All Existing Filegroups (filter by name pattern if needed)
+SELECT *
+FROM sys.filegroups
+WHERE type = 'FG'
